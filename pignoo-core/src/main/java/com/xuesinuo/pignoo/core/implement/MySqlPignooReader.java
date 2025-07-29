5741bf220a97a6b5bcb2f38478096b1a59f443fc
xuesinuo <nayukiminace@163.com>
2025-07-29 11:09:47 +0800

    过滤器增加条件过滤功能

--- pignoo-core/src/main/java/com/xuesinuo/pignoo/core/implement/MySqlPignooReader.java
+++ pignoo-core/src/main/java/com/xuesinuo/pignoo/core/implement/MySqlPignooReader.java
@@ -2,7 +2,6 @@
 package com.xuesinuo.pignoo.core.implement;
 
 import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
